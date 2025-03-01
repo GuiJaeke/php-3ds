bf7db1be4c20edf20e71e1ed9a8247e3c404685f
Gui Jaeke <107638175+GuiJaeke@users.noreply.github.com>
2025-02-28 22:47:32 -0300

    Add files via upload

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\top;
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(top $top)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, top $top)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(top $top)
+    {
+        //
+    }
+}
